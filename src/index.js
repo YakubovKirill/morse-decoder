8aa2657bcdf477d48a31a9f0b81fa8e5647fb31d
Yakubov Kirill <yakubov.kirill.work@gmail.com>
2020-10-02 00:05:22 +0300

    feat: create task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,49 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+let getElementsArr = (str = '', length = 1) => {
+    let codedLettersArr = []
+    let letter = ''
+    let counter = 0
+    str.split('').forEach(element => {
+          if (counter === (length - 1)) {
+              letter += element
+              codedLettersArr.push(letter)
+              counter = 0
+              letter = ''
+          } else {
+              letter += element
+              counter ++
+          }
+    });
+    return codedLettersArr
+}
+  
+const decode = (expr) => {
+      // write your solution here
+      let finalStr = ''
+      getElementsArr(expr, 10).forEach(element => {
+        let morseElem = '';
+  
+        if (element === '**********') finalStr += ' '
+        else {
+          getElementsArr(element, 2).forEach(elem => {
+            switch (elem) {
+              case '00': break
+              case '10': {
+                morseElem += '.'
+                break
+              }
+              case '11': {
+                morseElem += '-'
+                break
+              }
+            }
+          })
+          finalStr += MORSE_TABLE[morseElem]
+        }
+      });
+      return finalStr;
 }
 
 module.exports = {
